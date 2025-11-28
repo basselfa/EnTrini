5fd6737dfa519711c17d6fe2c15fcd431451cfc5
bassel.fatloun <bassel.fatloun@gitlab.com>
2025-11-28 20:39:40 +0100

    add tailwind

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import './styles.css';
 import Layout from './Layout';
 import Home from './Pages/Home';
 import Gyms from './Pages/Gyms';
